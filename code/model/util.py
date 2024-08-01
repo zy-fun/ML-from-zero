befdf080b06d3ff677c07ac8332d23bc97869023
zy-fun <2386756261@qq.com>
2024-08-01 15:34:28 +0800

    complete layernorm

--- code/model/util.py
+++ code/model/util.py
@@ -2,13 +2,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# Note: parameter 'unbiased' True for denominator equals N-1
+'''
+class:
+    LayerNorm(self, n_embd, eps)
+'''
+
+# This LayerNorm is rewrited for learning
+
+# Note: 'unbiased' parameter of tensor.var(): 
+# True for denominator equals N-1
 # and False for denominator equals N
 
 class LayerNorm(nn.Module):
     def __init__(self, n_embd, eps):
-        print(type(super()))
-        print(super())
         super().__init__()
         self.gamma = nn.Parameter(torch.ones(n_embd))
         self.beta = nn.Parameter(torch.zeros(n_embd))
@@ -16,7 +22,7 @@ class LayerNorm(nn.Module):
 
     def forward(self, x: torch.tensor):
         # x: [B, T, C]
-        x = x.float()   # not sure
+        # x = x.float()   # not sure
         mean = x.mean(dim=-1, keepdim=True)
         var = x.var(dim=-1, unbiased=False, keepdim=True)
         out = (x - mean) / torch.sqrt(var + self.eps)
@@ -26,8 +32,10 @@ class LayerNorm(nn.Module):
         
 if __name__ == "__main__":
     x = torch.tensor([[1, 2, 3],
-                      [4, 8, 12]])
-    layernorm = LayerNorm(3, 1e-5)
-    out = layernorm(x)
-    print(out)
-        
+                      [4, 8, 12]], dtype=torch.float32)
+    rewrited_layernorm = LayerNorm(3, 1e-5)
+    rewrited_out = rewrited_layernorm(x)
+    torch_layernorm = nn.LayerNorm(x.shape[-1]) 
+    torch_out = torch_layernorm(x)
+    print(rewrited_out)
+    print(torch_out)
