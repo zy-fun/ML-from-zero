da75e00f35241cb02295cb7ed514b6723713ba80
zy-fun <2386756261@qq.com>
2024-08-02 11:17:07 +0800

    complete encoder and decoder

--- code/model/transformer.py
+++ code/model/transformer.py
@@ -5,22 +5,74 @@ from util import *
 from attention import *
 
 # the dropout layer setting is a bit confusing
-class TransformerEncoderBlock(nn.Module):
+class EncoderBlock(nn.Module):
     def __init__(self, d_model, d_hidden, num_head, dropout=0.1):
         super().__init__()
         self.attention = MultiHeadAttention(d_model, num_head)
-        self.ffn = FeedForward(d_model, d_hidden, dropout=dropout)
+        self.ffn = FeedForward(d_model, d_hidden)
         self.norm1 = LayerNorm(d_model)
         self.norm2 = LayerNorm(d_model)
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
 
-    def forward(self, x, mask=None):
-        out = x + self.attention(x, x, x, mask)
+    def forward(self, x, mask):
+        out = x + self.dropout1(self.attention(x, x, x, mask))
         out = self.norm1(out)
-        out = out + self.ffn(out)
+
+        out = out + self.dropout2(self.ffn(out))
         out = self.norm2(out)
         return out
 
+class DecoderBlock(nn.Module):
+    def __init__(self, d_model, d_hidden, num_head, dropout=0.1):
+        super().__init__()
+        self.attention1 = MultiHeadAttention(d_model, num_head)
+        self.dropout1 = nn.Dropout(dropout)
+        self.norm1 = LayerNorm(d_model)
+
+        self.attention2 = MultiHeadAttention(d_model, num_head) # masked
+        self.dropout2 = nn.Dropout(dropout)
+        self.norm2 = LayerNorm(d_model)
+
+        self.ffn = FeedForward(d_model, d_hidden)
+        self.dropout3 = nn.Dropout(dropout)
+        self.norm3 = LayerNorm(d_model)
+
+    def forward(self, x, enc, mask=None):
+        x = x + self.dropout1(self.attention1(x, x, x, mask=mask))  # only attention1 enables mask
+        x = self.norm1(x)
+
+        x = x + self.dropout2(self.attention2(q=x, k=enc, v=enc))
+        x = self.norm2(x)
+
+        x = x + self.dropout3(self.ffn(x))
+        x = self.norm3(x)
+        return x
+    
+class Encoder(nn.Module):
+    def __init__(self, N, d_model, d_hidden, num_head, dropout=0.1):
+        super().__init__()
+        self.blocks = nn.ModuleList([
+            EncoderBlock(d_model, d_hidden, num_head, dropout) for _ in range(N)
+        ])
+
+    def forward(self, x):
+        for block in self.blocks:
+            x = block(x)
+        return x
+    
+class Decoder(nn.Module):
+    def __init__(self, N, d_model, d_hidden, num_head, dropout=0.1):
+        super().__init__()
+        self.blocks = nn.ModuleList([
+            DecoderBlock(d_model, d_hidden, num_head, dropout) for _ in range(N)
+        ])
+
+    def forward(self, x, enc, mask=None):
+        for block in self.blocks:
+            x = block(x, enc, mask=mask)
+        return x
+
 if __name__ == "__main__":
+
     pass
