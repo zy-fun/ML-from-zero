1c628172dcaf93d15349bc84d56ed9faaae0c0ea
zy-fun <2386756261@qq.com>
2024-08-02 11:59:48 +0800

    add some testing code

---
+++ code/config.py
@@ -0,0 +1,6 @@
+import torch
+
+N = 6
+d_model = 512
+d_ff = 2048
+num_head = 8
--- code/main.py
+++ code/main.py
@@ -1,12 +1,27 @@
 import torch
-from model.attention import *
+from model.transformer import *
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--N', type=int, default=6)
+parser.add_argument('--batch_size', type=int, default=4)
+parser.add_argument('--seq_len', type=int, default=100)
+parser.add_argument('--d_model', type=int, default=512)
+parser.add_argument('--d_ff', type=int, default=2048)
+parser.add_argument('--num_head', type=int, default=8)
+parser.add_argument('--dropout', type=float, default=0.1)
+parser.add_argument('--device', type=str, default='cuda')
+cfg = vars(parser.parse_args())
+
+N = cfg['N']
+batch_size = cfg['batch_size']
+seq_len = cfg['seq_len']
+d_model = cfg['d_model']
+d_ff = cfg['d_ff']
+num_head = cfg['num_head']
+dropout = cfg['dropout']
+device = cfg['device']
 
 if __name__ == "__main__":
-    b = 32
-    t = 100
-    embd_size = 64
-    device = 'cuda'
-    x = torch.ones(b, t, embd_size).to(device)
-    attention = MultiHeadAttention(embd_size, 8).to(device)
-    y = attention(x, x, x) 
-    print(x.shape, y.shape)
+    x = torch.ones(batch_size, seq_len, d_model).to(device)
+    encoder = Encoder(N, d_model, d_ff, num_head, dropout)
