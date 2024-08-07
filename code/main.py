5c76894c9bf31e26709125aad0a3c2806ab8cbc7
zy-fun <2386756261@qq.com>
2024-08-07 16:31:39 +0800

    complete TokenMapper

--- code/main.py
+++ code/main.py
@@ -1,6 +1,8 @@
 import torch
 from model.transformer import *
+from model.tokenizer import *
 import argparse
+import tiktoken
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--N', type=int, default=6)
@@ -27,11 +29,30 @@ eps = cfg['eps']
 device = cfg['device']
 
 if __name__ == "__main__":
+    encoder = tiktoken.get_encoding("cl100k_base")
+    text = """Text-to-image models offer unprecedented freedom to guide creation through natural language. Yet, it is unclear how such freedom can be exercised to generate images of specific unique concepts, modify their appearance, or compose them in new roles and novel scenes. In other words, we ask: how can we use language-guided models to turn our cat into a painting, or imagine a new product based on our favorite toy? Here we present a simple approach that allows such creative freedom. Using only 3-5 images of a user-provided concept, like an object or a style, we learn to represent it through new "words" in the embedding space of a frozen text-to-image model. These "words" can be composed into natural language sentences, guiding personalized creation in an intuitive way. Notably, we find evidence that a single word embedding is sufficient for capturing unique and varied concepts. We compare our approach to a wide range of baselines, and demonstrate that it can more faithfully portray the concepts across a range of applications and tasks.
+Our code, data and new words will be available at: this https URL"""
+
+    tokens = encoder.encode(text)
+    print(tokens)
+    mapper = TokenMapper(tokens)
+
+    enc_tokens = mapper.encode(tokens)
+    dec_tokens = mapper.decode(enc_tokens)
+
+    print(set(enc_tokens), len(mapper))
+    print(dec_tokens == tokens)
+    dec_text = encoder.decode(dec_tokens)
+
+    print(text == dec_text)
+
     enc = torch.randint(0, vocab_size, (batch_size, seq_len)).to(device)
     data = torch.randint(0, vocab_size, (batch_size, seq_len + 1)).to(device)
+
     x, y = data[:, :seq_len], data[:, 1:]
 
     model = Transformer(N, vocab_size, d_model, d_ff, num_head, dropout=dropout).to(device)
 
     logits = model(x, enc)
+
     print(logits.shape)
---
+++ code/model/tokenizer.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+"""
+class:
+    TokenMapper(self, text)
+"""
+
+class TokenMapper:
+    """
+        Mapping arbitrary tokens to integer range(0, N)
+    """
+    def __init__(self, tokens=[]):
+        self.__encoder = {}
+        self.__decoder = {}
+        self.__id = 0
+        self.append(tokens)
+
+    def __len__(self):
+        return len(self.__encoder) + 1    # extra one token for <unknown>
+
+    def append(self, tokens):
+        for token in set(tokens):
+            if token not in self.__encoder:
+                self.__encoder[token] = self.__id
+                self.__decoder[self.__id] = token
+                self.__id += 1
+
+    def encode(self, tokens):
+        # return a special value for unknown token which can be decoded as 'unknown'
+        encoded = [self.__encoder.get(token, len(self.__encoder)) for token in tokens]
+        return encoded
+
+    def decode(self, encoded, unknown=None):
+        # return value 'unknown' for unknown token
+        decoded = [self.__decoder.get(encoded_token, unknown) for encoded_token in encoded]
+        return decoded
+    
+if __name__ == "__main__":
+    tokens = [1199, 4791, 14064, 4211, 3085, 31069, 11542, 311, 8641, 9886, 1555, 5933, 4221, 13, 14968, 11, 433, 374, 25420, 1268, 1778, 11542, 649, 387, 62113, 311, 7068, 5448, 315, 3230, 5016, 19476, 11, 5719, 872, 11341, 11, 477, 31435, 1124, 304, 502, 13073, 323, 11775, 16451, 13, 763, 1023, 4339, 11, 584, 2610, 25, 1268, 649, 584, 1005, 4221, 84792, 291, 4211, 311, 2543, 1057, 8415, 1139, 264, 19354, 11, 477, 13085, 264, 502, 2027, 3196, 389, 1057, 7075, 22068, 30, 5810, 584, 3118, 264, 4382, 5603, 430, 6276, 1778, 11782, 11542, 13, 12362, 1193, 220, 18, 12, 20, 5448, 315, 264, 1217, 10039, 44057, 7434, 11, 1093, 459, 1665, 477, 264, 1742, 11, 584, 4048, 311, 4097, 433, 1555, 502, 330, 5880, 1, 304, 279, 40188, 3634, 315, 264, 20268, 1495, 4791, 14064, 1646, 13, 4314, 330, 5880, 1, 649, 387, 24306, 1139, 5933, 4221, 23719, 11, 51346, 35649, 9886, 304, 459, 42779, 1648, 13, 2876, 2915, 11, 584, 1505, 6029, 430, 264, 3254, 3492, 40188, 374, 14343, 369, 40880, 5016, 323, 28830, 19476, 13, 1226, 9616, 1057, 5603, 311, 264, 7029, 2134, 315, 3122, 11243, 11, 323, 20461, 430, 433, 649, 810, 94176, 25920, 279, 19476, 4028, 264, 2134, 315, 8522, 323, 9256, 627, 8140, 2082, 11, 828, 323, 502, 4339, 690, 387, 2561, 520, 25, 420, 3788, 5665]
+    
+    pass
