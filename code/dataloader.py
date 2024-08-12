5f7123049b79c0840b7c5b288d7b83fa3f6a1692
zy-fun <2386756261@qq.com>
2024-08-12 16:51:19 +0800

    complement StockDataset api

--- code/dataloader.py
+++ code/dataloader.py
@@ -5,7 +5,13 @@ from tqdm import tqdm
 import pdb
 from torch.utils.data import Dataset
 
+"""
+class:
+    StockDataset(dirname, lazy_load)
 
+function:
+    download_stock(dirname)
+"""
 
 class StockDataset(Dataset):
     """
@@ -18,21 +24,32 @@ class StockDataset(Dataset):
             ...
             sz301608.csv
     """
-    def __init__(self, dirname):
+    def __init__(self, dirname, lazy_load=True):
         super().__init__()
         self.dirname = dirname
         self.stock_list = pd.read_csv(os.path.join(dirname, 'stock_list.csv'))
         self.symbols = self.stock_list['代码'].tolist()
+        
+        self.data = [None for _ in range(len(self.symbols))]
+        if not lazy_load:
+            for i, symbol in enumerate(self.symbols):
+                stock = pd.read_csv(os.path.join(dirname, f"{symbol}.csv"))
+                self.data[i] = stock
 
     def __len__(self):
         return len(self.symbols)
 
     def __getitem__(self, idx):
-        pass
+        if self.data[idx] is None:
+            symbol = self.symbols[idx]
+            stock = pd.read_csv(os.path.join(dirname, f"{symbol}.csv"))
+            self.data[idx] = stock
+        return self.data[idx]
 
-
-
-def data_downloader(dirname='./dataset'):
+def download_stock(dirname='./dataset'):
+    """
+        Download stock data through akshare to given directory.
+    """
     # 1. get the symbol list of stocks
     stock_list_path = os.path.join(dirname, 'stock_list.csv')
 
@@ -56,7 +73,7 @@ def data_downloader(dirname='./dataset'):
 if __name__ == "__main__":
     dirname = './dataset'
 
-    # data_downloader(dirname)
+    # download_stock(dirname)
 
     ratios = [.9, .05, .05]
     dataset = StockDataset(dirname)
