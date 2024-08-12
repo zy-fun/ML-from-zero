e738ede83b059e8a5e2111ef1d2829cf6f12a377
zy-fun <2386756261@qq.com>
2024-08-12 15:19:37 +0800

    updata main func of dataloader.py

--- code/dataloader.py
+++ code/dataloader.py
@@ -1,10 +1,11 @@
 import akshare as ak
 import os
 import pandas as pd
+from tqdm import tqdm
 import pdb
 
 if __name__ == "__main__":
-    # 1. get the code list of stocks
+    # 1. get the symbol list of stocks
     stock_list_path = './dataset/stock_list.csv'
 
     if os.path.exists(stock_list_path):
@@ -13,6 +14,11 @@ if __name__ == "__main__":
         stock_list = ak.stock_zh_a_spot()   # may fail due to connection problem
         stock_list.to_csv(stock_list_path, index=False) 
 
-    code_list = stock_list['代码']
-
-    # 2. wait to do
+    # 2. download stock data for every symbol
+    for symbol in tqdm(stock_list['代码']):
+        if os.path.exists(f"./dataset/{symbol}.csv"):
+            continue
+        else:
+            # adjust 'qhq' means for '前复权'
+            stock_data = ak.stock_zh_a_hist(symbol=symbol[2:], period="daily", adjust="qfq")
+            stock_data.to_csv(f"./dataset/{symbol}.csv", index=False)
