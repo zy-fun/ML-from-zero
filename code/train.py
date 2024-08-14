9e934579c20f04920600904d8da722fc6176685b
zy-fun <2386756261@qq.com>
2024-08-14 15:36:12 +0800

    complete cfgs in train.py

--- code/train.py
+++ code/train.py
@@ -1,18 +1,28 @@
 import torch
 from torch.utils.data import DataLoader
-# from model.transformer import *
-# from model.tokenizer import *
 from model.RegressiveTransformer import RegressiveTransformer
 from model.util import get_sequence_mask
 from dataloader import StockDataset
 import argparse
 import akshare as ak
 import mplfinance as mpf
+import time
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--num_epoch', type=int, default=1000)
+
+# environment setting
+parser.add_argument('--seed', type=int, default=42)
+parser.add_argument('--device', type=str, default='cuda')
+
+# train setting
 parser.add_argument('--max_iter', type=int, default=1e6)
+parser.add_argument('--eval_iter', type=int, default=1e3)
+parser.add_argument('--batch_per_iter', type=int, default=1000)
+parser.add_argument('--num_epoch', type=int, default=1000)
+parser.add_argument('--load_model', type=str, default=None)
+
+# model setting
 parser.add_argument('--N', type=int, default=6)
 parser.add_argument('--batch_size', type=int, default=4)
 parser.add_argument('--vocab_size', type=int, default=1000)
@@ -22,9 +32,16 @@ parser.add_argument('--d_ff', type=int, default=2048)
 parser.add_argument('--num_head', type=int, default=8)
 parser.add_argument('--dropout', type=float, default=0.1)
 parser.add_argument('--eps', type=float, default=1e-5)
-parser.add_argument('--device', type=str, default='cuda')
+
+# file path setting
+parser.add_argument('--dataset_dirname', type=str, default='./dataset')
+parser.add_argument('--log_dirname', type=str, default='./log')
+parser.add_argument('--save_dirname', type=str, default='./saved_model')
 cfg = vars(parser.parse_args())
 
+max_iter = cfg['max_iter']
+eval_iter = cfg['eval_iter']
+batch_per_iter = cfg['batch_per_iter']
 num_epoch = cfg['num_epoch']
 N = cfg['N']
 batch_size = cfg['batch_size']
@@ -38,15 +55,15 @@ eps = cfg['eps']
 device = cfg['device']
 
 if __name__ == "__main__":
+    d_feature = 4   # open, close, high, low
     model = RegressiveTransformer(N, 4, d_model, d_ff, num_head).to(device)
 
-    dataset = StockDataset("./dataset", start=0, end=0.01, blocksize=seq_len)
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-    mask = get_sequence_mask(seq_len).to(device)
-    for data_x, data_y in tqdm(dataloader):
-        data_x = data_x.permute(0,2,1).to(device)
-        data_y = data_y.permute(0,2,1).to(device)
-        break
-    print(data_x.shape)
-    output = model(data_x, mask)
-    print(output.shape)
+    train_portion = 0.9
+    val_portion = 0.1
+    dataset = StockDataset("./dataset", start=0, end=train_portion, blocksize=seq_len)
+    train = DataLoader(dataset, batch_size=4, shuffle=True)
+    
+    model.train()
+    for iter in range(max_iter):
+        if iter % eval_iter == 0:
+            pass
